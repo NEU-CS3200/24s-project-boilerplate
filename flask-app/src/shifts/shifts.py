2effafe1103430b8c43ad5cf0f9447b1e06d9626
gemackin <gmackin617@gmail.com>
2024-04-17 15:16:30 -0400

    Add 6th route to shifts

--- flask-app/src/shifts/shifts.py
+++ flask-app/src/shifts/shifts.py
@@ -1,51 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response, current_app
-import json
-from src import db
-
-
-def execute(query, commit = False):
-    current_app.logger.info(query)
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    if commit: db.get_db().commit()
-    return cursor
-
-
-def get_helper(query):
-    cursor = execute(query)
-    cols = [x[0] for x in cursor.description]
-    data = [dict(zip(cols, row)) for row in cursor.fetchall()]
-    return jsonify(data)
-
-
-def post_helper(table, return_data = False):
-    def to_str(val):
-        s = isinstance(val, str)
-        return ('\'{}\'','{}')[s].format(val)
-    data = request.json
-    current_app.logger.info(data)
-    cols = ', '.join(data.keys())
-    vals = ', '.join([to_str(v) for v in data.values()])
-    query = f'INSERT INTO {table} ({cols}) VALUES ({vals})'
-    execute(query, commit = True)
-    return data if return_data else 'Success!'
-
-
-def put_helper(table, val, key = 'id', data = None, return_data = False):
-    if data is None: data = request.json
-    current_app.logger.info(data)
-    pairs = ', '.join([f'{k} = {v}' for k, v in data.items() if k != key])
-    query = f'UPDATE {table} SET {pairs} WHERE {key} = {val}'
-    execute(query, commit = True)
-    return data if return_data else 'Success!'
-
-
-def delete_helper(table, val, key = 'id'):
-    query = f'DELETE FROM {table} WHERE {key} = {val}'
-    execute(query, commit = True)
-    return 'Success!'
-
-
+from src.blueprint_template import *
 shifts = Blueprint('shifts', __name__)
 
 
@@ -55,7 +8,8 @@ def get_user_shifts(id):
     query = f'SELECT Sh.*, L.address1 FROM Shifts Sh \
         JOIN Schedules Sc ON Sh.schedule = Sc.id \
         JOIN Locations L ON Sc.location = L.id \
-        WHERE Sh.employee = {id}'
+        WHERE Sh.employee = {id} \
+        ORDER BY dayOfWeek, startTime, endTime'
     return get_helper(query)
 
 
@@ -85,3 +39,14 @@ def put_shift(id):
 @shifts.route('/shifts/<id>', methods = ['DELETE'])
 def delete_shift(id):
     return delete_helper('Shifts', id)
+
+
+# Transfer all of an employee's shifts to another employee
+@shifts.route('/transferShifts', methods = ['PUT'])
+def transfer_shifts(id):
+    if data is None: data = request.json
+    current_app.logger.info(data)
+    id1, id2 = data['fromEmployee'], data['toEmployee']
+    query = f'UPDATE Shifts SET employee = {id2} WHERE employee = {id1}'
+    execute(query, commit = True)
+    return 'Success!'
